b62ca9b00ecc48adf0cd074c0784ca3d115f5dc5
Chun Ly <astro.chun@gmail.com>
2017-02-23 13:51:23 -0700

    mmtcam.find_stars: Add option to plot images and overlay sources

--- mmtcam.py
+++ mmtcam.py
@@ -25,18 +25,32 @@ from astropy.table import Table
 from astropy.stats import sigma_clipped_stats
 from photutils import DAOStarFinder
 
-def get_files(path0):
-    files = glob.glob(path0+'/*fits*')
-    return files
-#enddef
+from astropy.visualization import SqrtStretch
+from astropy.visualization.mpl_normalize import ImageNormalize
+from photutils import CircularAperture
+norm = ImageNormalize(stretch=SqrtStretch())
+
+from matplotlib.backends.backend_pdf import PdfPages
 
 def get_seqno(files):
     t_files = [os.path.basename(file) for file in files]
     seqno = [file.replace('.fits.gz','').replace('.fits','').split('.')[1] for
              file in t_files]
-    return seqno
+    return np.array(seqno)
 
-def find_stars(files=None, path0=None, silent=False, verbose=True):
+def get_files(path0):
+    # Later Mod on 23/02/2017
+    files = glob.glob(path0+'/*fits*')
+    seqno = get_seqno(files) # Later + on 23/02/2017
+
+    s_idx = np.argsort(np.array(seqno))
+
+    files = np.array(files)[s_idx]
+    return files, seqno[s_idx]
+#enddef
+
+def find_stars(files=None, path0=None, plot=False, out_pdf_plot=None,
+               silent=False, verbose=True):
     '''
     Find stars in an image and return a catalog of positions
 
@@ -71,12 +85,20 @@ def find_stars(files=None, path0=None, silent=False, verbose=True):
         return
 
     if files == None and path0 != None:
-        files = get_files(path0)
+        files, seqno = get_files(path0)
         path0 = None # Reset since files will have full path
+    else:
+        seqno = get_seqno(files)
 
-    seqno = get_seqno(files)
+    if path0 == None:
+        s_date = os.path.dirname(files[0]).split('/')[-1]
 
-    for ff in [0]: #xrange(len(files)):
+    if plot == True:
+        if out_pdf_plot == None:
+            out_pdf_plot = os.path.dirname(files[0])+'/find_stars.pdf'
+        pp = PdfPages(out_pdf_plot)
+
+    for ff in xrange(len(files)):
         image = fits.getdata(files[ff])
         mean, median, std = sigma_clipped_stats(image, sigma=3.0, iters=5)    
         if verbose == True:
@@ -86,10 +108,35 @@ def find_stars(files=None, path0=None, silent=False, verbose=True):
 
         # Exclude saturated objects
         unsat = np.where(s_cat['peak'] <= 60000.0)[0]
+        sat   = np.where(s_cat['peak'] > 60000.0)[0]
+        cat_sat = s_cat[sat]
         s_cat = s_cat[unsat]
         s_cat.sort(['peak'])
         s_cat.reverse()
         if ff == 0: s_cat.pprint()
+
+        if plot == True:
+            pos0  = (s_cat['xcentroid'], s_cat['ycentroid'])
+            aper0 = CircularAperture(pos0, r=8.)
+
+            pos0      = (cat_sat['xcentroid'], cat_sat['ycentroid'])
+            sat_aper0 = CircularAperture(pos0, r=8.)
+
+            fig, ax = plt.subplots()
+            ax.imshow(image-median, cmap='Greys', origin='lower', norm=norm)
+            aper0.plot(color='blue', lw=1.5, alpha=0.5)
+            sat_aper0.plot(color='red', lw=1.5, alpha=0.5)
+
+            t_ann = s_date+'/'+os.path.basename(files[ff])
+            ax.annotate(t_ann, [0.025,0.975], xycoords='axes fraction',
+                         ha='left', va='top')
+            fig.savefig(pp, format='pdf', bbox_inches='tight')
+
+    #endfor
+    if plot == True:
+        if silent == False:
+            log.info('## Writing : '+out_pdf_plot+' | '+systime())
+        pp.close()
 
     if silent == False: log.info('### End: '+systime())
 #enddef
@@ -129,7 +176,7 @@ def run_all(files=None, path0=None, silent=False, verbose=True):
     if silent == False: log.info('### Begin: '+systime())
 
     if files == None and path0 != None:
-        files = get_files(path0)
+        files, seqno = get_files(path0)
         path0 = None # Reset since files will have full path
 
     if silent == False:
@@ -137,6 +184,6 @@ def run_all(files=None, path0=None, silent=False, verbose=True):
         log.info(path0)
         for file in files: log.info(os.path.basename(file))
 
-    find_stars(files=files, path0=path0)
+    find_stars(files=files, path0=path0, plot=True)
 
     if silent == False: log.info('### End: '+systime())
