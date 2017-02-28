c72ca347f1b7d063049d66ca2e0422bd57fca626
Chun Ly <astro.chun@gmail.com>
2017-02-27 23:46:37 -0700

    Update __init__.py for package requirements

--- __init__.py
+++ __init__.py
@@ -5,7 +5,13 @@ MMTtools
 Set of Python 2.7 codes to use with data from MMT
 
 Requirements:
- XXX
+ astropy    >=v1.3
+ chun_codes
+ matplotlib >=v1.5.3
+ photutils  >=v0.3
+ scipy      >= 0.18.1
+ ccdproc    >= v1.2.0
+ astroquery >= 0.3.2
 
 How to use the documentation
 ----------------------------
@@ -13,3 +19,5 @@ import MMTtools as MMT
 """
 
 import mmtcam
+
+reload(mmtcam)
